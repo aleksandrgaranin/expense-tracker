127cc6438402f0ab679038c292c9c3affa4e3401
aleksandrgaranin <aleksandrgaranin89@gmail.com>
2021-05-16 16:19:36 -0700

    cntext, state flow, delete and add logic

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import {
   TextField,
   Typography,
@@ -9,10 +9,31 @@ import {
   Select,
   MenuItem,
 } from "@material-ui/core";
+
+import { ExpenseTrackerContext } from "../../../context/context";
+import {v4 as uuidv4} from 'uuid'
 import useStyles from "./styles";
+
+const initialState = {
+  amount: "",
+  category: "",
+  type: "Income",
+  date: new Date(),
+};
 
 const Form = () => {
   const classes = useStyles();
+  const { addTransaction } = useContext(ExpenseTrackerContext);
+  const [formData, setFormData] = useState(initialState);
+
+  const createTransaction =() => {    
+    const transaction = { ...formData, amount: Number(formData.amount), id: uuidv4() }
+    addTransaction(transaction);
+    setFormData(initialState)
+  }
+
+
+  // console.log(formData);
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
@@ -23,7 +44,10 @@ const Form = () => {
       <Grid item xs={6}>
         <FormControl fullWidth>
           <InputLabel>Type</InputLabel>
-          <Select>
+          <Select
+            value={formData.type}
+            onChange={(e) => setFormData({ ...formData, type: e.target.value })}
+          >
             <MenuItem value="Income">Income</MenuItem>
             <MenuItem value="Expense">Expense</MenuItem>
           </Select>
@@ -32,19 +56,44 @@ const Form = () => {
       <Grid item xs={6}>
         <FormControl fullWidth>
           <InputLabel>Category</InputLabel>
-          <Select>
+          <Select
+            value={formData.category}
+            onChange={(e) =>
+              setFormData({ ...formData, category: e.target.value })
+            }
+          >
             <MenuItem value="business">business</MenuItem>
-            <MenuItem value="business">business</MenuItem>
+            <MenuItem value="salary">salary</MenuItem>
           </Select>
         </FormControl>
       </Grid>
       <Grid item xs={6}>
-        <TextField type="number" label="Amount" fullWidth />
+        <TextField
+          type="number"
+          label="Amount"
+          fullWidth
+          value={formData.amount}
+          onChange={(e) => setFormData({ ...formData, amount: e.target.value })}
+        />
       </Grid>
       <Grid item xs={6}>
-        <TextField type="date" label="Date" fullWidth />
+        <TextField
+          type="date"
+          label="Date"
+          fullWidth
+          value={formData.date}
+          onChange={(e) => setFormData({ ...formData, date: e.target.value })}
+        />
       </Grid>
-      <Button className={classes.button} variant="outlined" color="primary" fullWidth>Submit</Button>
+      <Button
+        className={classes.button}
+        variant="outlined"
+        color="primary"
+        fullWidth
+        onClick={createTransaction}
+      >
+        Submit
+      </Button>
     </Grid>
   );
 };
--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import {
   List as MUIList,
   ListItem,
@@ -11,16 +11,41 @@ import {
 } from "@material-ui/core";
 import { Delete, MoneyOff } from "@material-ui/icons";
 
+import { ExpenseTrackerContext } from "../../../context/context";
+
 import useStyles from "./styles";
 
 const List = () => {
   const classes = useStyles();
-  const transactions = [
-      {id:1, type: "Income", category:"Salary", amount: 50, date: 'Sun May 16 2021'},
-      {id:2, type: "Expense", category:"Coffe", amount: 5, date: 'Sun May 16 2021'},
-      {id:3, type: "Income", category:"Salary", amount: 150, date: 'Sun May 16 2021'},
+  const { deleteTransaction, transactions } = useContext(ExpenseTrackerContext);
+  // console.log(deleteTransaction);
 
-  ];
+  // const transactions = [
+  //   {
+  //     id: 1,
+  //     type: "Income",
+  //     category: "Salary",
+  //     amount: 50,
+  //     date: "Sun May 16 2021",
+  //   },
+  //   {
+  //     id: 2,
+  //     type: "Expense",
+  //     category: "Coffe",
+  //     amount: 5,
+  //     date: "Sun May 16 2021",
+  //   },
+  //   {
+  //     id: 3,
+  //     type: "Income",
+  //     category: "Salary",
+  //     amount: 150,
+  //     date: "Sun May 16 2021",
+  //   },
+  // ];
+  const deleteHandler = (trId) => {
+    deleteTransaction(trId)
+  }
 
   return (
     <MUIList dense={false} className={classes.list}>
@@ -44,11 +69,14 @@ const List = () => {
                 <MoneyOff />
               </Avatar>
             </ListItemAvatar>
-            <ListItemText primary={transaction.category} secondary={`$${transaction.amount} - ${transaction.date}`}/>
+            <ListItemText
+              primary={transaction.category}
+              secondary={`$${transaction.amount} - ${transaction.date}`}
+            />
             <ListItemSecondaryAction>
-                <IconButton edge='end' aria-label="delete" onClick="">
-                    <Delete/>
-                </IconButton>
+              <IconButton edge="end" aria-label="delete" onClick={()=> deleteHandler(transaction.id)}>
+                <Delete />
+              </IconButton>
             </ListItemSecondaryAction>
           </ListItem>
         </Slide>
