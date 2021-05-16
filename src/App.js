1e10e26ae5651051bfc0e1fb6ccdc981d2737e30
aleksandrgaranin <aleksandrgaranin89@gmail.com>
2021-05-16 12:03:44 -0700

    Detail component added + Styling custom hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react'
-import './index.css'
+import React from "react";
+
+import { Grid } from "@material-ui/core";
+import Detals from "./components/Details/Details";
+import useStyles from './styles'
 
 const App = () => {
-    return (
-        <div>
-            App.
-        </div>
-    )
-}
+    const classes = useStyles()
+  return (
+    <div>
+        <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{ height: '100vh'}}>
+            <Grid item xs={12} sm={4}>
+                <Detals title='Income'/>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+                Main
+            </Grid>
+            <Grid item xs={12} sm={4}>
+                <Detals title='Expense' />
+            </Grid>
+        </Grid>
+    </div>
+    );
+};
 
-export default App
+export default App;
